b8831fc60ae0b1c42f615fba2830d982e4e32eae
Mustafa-abdelhamid <90484856+Mustafa-abdelhamid@users.noreply.github.com>
2022-02-27 00:37:46 +0200

    Update top_part_three.v

--- RTL/top_part_three.v
+++ RTL/top_part_three.v
@@ -1,45 +1,122 @@
-module top_part_three (Sign_in , roundMode , A , B , sub ,
- E_out , ovf , ovf_rnd , final_shift_left ,
- rightpath_exponent_update_control ,
- overFlow , inexact_flag,most_bits_of_adder_out,
- underflow_flag , invalid_flag,
- final_M_out , final_E_out);
+module Top_Add_Sub (
 
-input Sign_in;
-input [1:0]roundMode;
-input [26:0]A , B;
-input sub, overFlow , underflow_flag , invalid_flag;
-input [7:0]E_out;
-output  inexact_flag;
-output  ovf , ovf_rnd;
-output  [4:0]final_shift_left;
-output  [1:0]rightpath_exponent_update_control;
-output  [2:0]most_bits_of_adder_out;
+input     wire   [7:0]        Ex,Ey,
+input     wire   [22:0]       Mx,My,
+input     wire                Sx,Sy,EOP,sub,
+input	  wire	 [1:0] 		  roundMode , ////
+output    wire   [7:0]        Ez,
+output    wire   [22:0]       Mz,
+output    wire                Sz,
+output	  wire  invalid_flag,overflow_flag,underflow_flag,inexact_flag,zero_flag ////////
+); 
 
-output  [22:0] final_M_out;
-output  [7:0] final_E_out; 
-//internal signals
-wire [26:0]sum;
-wire [26:0]E;
-wire [4:0]SHL;
-wire [26:0]sum_shifted;
-wire [22:0]correct_sum_shifted;
-wire second_shift_left;
-wire [26:0]righPass_shift_out;
-wire [22:0]MOut;
-wire underflow_flag , invalid_flag;
-wire [22:0]Mz;
-//instances
-adder_modified adder_mod_inst (A , B , sub , sum , ovf);
-assign most_bits_of_adder_out = {ovf , sum[26:25]};
+wire  	    cmp;
+wire        sgn_d;
+wire        zero_d;
+wire [7:0]  d;
+wire [26:0] out_11;
+wire [26:0] out_22;
 
-LZA_1_modified LZA_1_modified_inst (A , B , E);
-LZA_2_modified LZA_2_modified_inst (E,SHL);
-first_massive_shift_left first_massive_shift_left_inst (sum , SHL , sum_shifted);
-LZA_final_modified LZA_final_modified_inst (sum_shifted , correct_sum_shifted , second_shift_left);
-shift_left_adder shift_left_adder_inst (SHL , second_shift_left , final_shift_left);
-left_right_shifter left_right_shifter_inst (sum , ovf , righPass_shift_out , rightpath_exponent_update_control);
-ROUND ROUND_inst (righPass_shift_out , roundMode , Sign_in , MOut , ovf_rnd , inexact_flag);
-mantessa_mux mantessa_mux_inst (correct_sum_shifted , MOut , most_bits_of_adder_out , Mz);
-final_output final_output_inst (Mz , E_out , final_shift_left , overFlow , underflow_flag , invalid_flag , final_M_out , final_E_out);
-endmodule
+wire [7:0]  Out_mux;
+
+wire        ovf;
+wire        ovf_rnd;
+wire 		[7:0] Ez_pre ; 
+wire max_exponent_z ;
+
+wire [4:0]final_shift_left; 
+wire [1:0]rightpath_exponent_update_control;
+wire  [2:0]  most_bits_of_adder_out ;
+Alignment A1 (
+.Mx(Mx),
+.My(My),
+.sgn_d(sgn_d), 
+.EOP(EOP),
+.zero_d(zero_d),
+.Ex(Ex),
+.Ey(Ey),
+.d(d),
+.out_11(out_11),
+.out_22(out_22),
+.Cmp(cmp)
+);
+
+Exponent_Difference Ex_Diff (
+.Ex(Ex),
+.Ey(Ey),
+.sgn_d(sgn_d),
+.zero_d(zero_d),
+.d(d)
+);
+
+
+Mux mux11 (
+.Ex(Ex),
+.Ey(Ey),
+.sgn_d(sgn_d),
+.Out_mux(Out_mux)
+);
+Exponent_shifter Ex_shifter (
+.E (Out_mux),
+.L_shift_value (final_shift_left), 
+.selection ( ~| most_bits_of_adder_out),
+.L_or_R(rightpath_exponent_update_control) ,
+.Ez_pre(Ez_pre)
+);
+Exponent_Update Ex_update (
+.Mux_Out(Ez_pre),
+.Ez(Ez),
+.ovf(ovf),
+.ovf_rnd(ovf_rnd)
+);
+assign max_exponent_z= &Ez ; 
+
+Sign_of_z sign_z11 (
+.Sx(Sx),
+.Sy(Sy),
+.EOP(EOP),
+.cmp(cmp),
+.sign_d(sgn_d),
+.zero_d(zero_d),
+.Sz(Sz)
+);
+
+exceptions exceptions_block
+ (.Ex (Ex),
+ .Ey(Ey),
+ .Mx(Mx),
+ .My(My),
+ .Mz(Mz),
+ .EOP(EOP),
+ .exponent_z(Ez),
+ .required_shift(final_shift_left) ,
+ .invalid_flag(invalid_flag),
+ .overflow_flag(overflow_flag),
+ .underflow_flag(underflow_flag),
+ .inexat_flag(inexat_flag),
+ .zero_flag(zero_flag),
+ .max_exponent_z(max_exponent_z)
+ );
+ 
+top_part_three omar (
+.Sign_in(Sz) ,
+.roundMode(roundMode) ,
+.A(out_11) ,
+.B(out_22) ,
+.sub (sub) ,
+.E_out (Ez),
+.ovf(ovf) , 
+.ovf_rnd(ovf_rnd) ,
+.final_shift_left(final_shift_left) , 
+.rightpath_exponent_update_control(rightpath_exponent_update_control) ,
+.overFlow(overflow_flag) ,
+.inexact_flag(inexact_flag) , 
+.most_bits_of_adder_out (most_bits_of_adder_out) ,
+.final_M_out(Mz) ,
+.final_E_out (Ez) ,
+.underflow_flag(underflow_flag) ,
+.invalid_flag(invalid_flag)
+
+ );
+ 
+ endmodule
