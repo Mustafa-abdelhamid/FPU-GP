f788d72ef7662998b6dd01fef6f52c15042baecf
Mohamed Saleh <100477828+7mohamedsaleh@users.noreply.github.com>
2022-03-05 17:50:14 +0200

    Update Exceptions.v

--- RTL/Multiplier v3/Exceptions.v
+++ RTL/Multiplier v3/Exceptions.v
@@ -1,19 +1,30 @@
- `timescale 1us/1us 
 module exceptions (
 		input wire [7:0]Ex,Ey,Ez,
-		input wire [22:0]Mx,My,Mz,
+		input wire [22:0] Mx,My,
+		input wire [23:0] Mz,		
 		input wire [4:0]required_shift,
-		output reg  invalid_flag,overflow_flag,underflow_flag,zero_flag
-						);
+		input wire [4:0] mantissaReqiredModify,
+		input wire          overflow_case ,
+		output reg  invalid_flag,overflow_flag,zero_flag
+					);
 
 reg zero_mantessa_x,zero_mantessa_y,max_exponent_x,max_exponent_y,x_is_inf,y_is_inf,min_exponent_z,zero_mantessa_z;
 reg zero_exponent_x,zero_exponent_y,max_exponent_z,x_is_zero,y_is_zero,x_is_NAN,y_is_NAN;
-reg [7:0]internal_subtract;
+//reg [7:0]internal_subtract;
+
+wire        zero_Ex,zero_Ey;
+wire [23:0] Mx1,My1;
+
+assign zero_Ex = ~| Ex ;
+assign zero_Ey = ~| Ey ;
+
+assign Mx1 = zero_Ex ? {1'b0,Mx} : {1'b1,Mx} ;
+assign My1 = zero_Ex ? {1'b0,My} : {1'b1,My} ;
 
 always @(*)
 	begin
-			zero_mantessa_x = ~| Mx;
-			zero_mantessa_y = ~| My;
+			zero_mantessa_x = ~| Mx1;
+			zero_mantessa_y = ~| My1;
 			zero_mantessa_z = ~| Mz;
 			
 			zero_exponent_x = ~| Ex;
@@ -39,11 +50,11 @@ always @(*)
 			
 			zero_flag = (	x_is_zero & !y_is_inf	)	||	(	!x_is_inf & y_is_zero	) ;
 			
-			overflow_flag = (max_exponent_z & zero_mantessa_z) || (	(x_is_inf & !y_is_zero) | (!x_is_zero & y_is_inf)	) ;
+			overflow_flag = (max_exponent_z & zero_mantessa_z) || (	(x_is_inf & !y_is_zero) | (!x_is_zero & y_is_inf)) || overflow_case ;
 			
 			invalid_flag = (x_is_zero & y_is_inf)	||	(x_is_inf & y_is_zero) 	||  (x_is_NAN ||y_is_NAN) ;
 			
-			{underflow_flag,internal_subtract} = (Ez + ~required_shift + 1'b1);//
+			//{underflow_flag,internal_subtract} = (Ez + ~required_shift + 1'b1);//
 
 			
 			
